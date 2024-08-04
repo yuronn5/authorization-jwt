f0fcca9072f56dc0dde68cb745f7cbd2723737b8
yurii bilous <yurii.b@elogic.com.ua>
2024-08-04 11:58:51 +0200

    controllers for routing

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,51 @@
+class UserController {
+  async registration(req, res, next) {
+    try {
+
+    } catch (error) {
+        
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+
+    } catch (error) {
+        
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+
+    } catch (error) {
+        
+    }
+  }
+
+  async activate(req, res, next) {
+    try {
+
+    } catch (error) {
+        
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+
+    } catch (error) {
+        
+    }
+  }
+
+  async getUsers(req, res, next) {
+    try {
+        res.json("users1");
+    } catch (error) {
+        
+    }
+  }
+}
+
+module.exports = new UserController();
