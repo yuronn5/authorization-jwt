bb8868265bc65896ca6c0b6cad5f2f0ff387521d
yurii bilous <yurii.b@elogic.com.ua>
2024-08-04 11:06:36 +0200

    connect our mongose db

--- server/index.js
+++ server/index.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 const start = async () => {
   try {
+    await mongoose.connect(process.env.DB_URL, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true
+    });
+    console.log("MongoDB connected");
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
     })
