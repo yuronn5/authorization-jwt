d2efeef9c2cf9df39aec5bf3751f8924f02f768a
yurii bilous <yurii.b@elogic.com.ua>
2024-08-04 15:52:31 +0200

    added validation for request

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,14 @@
 const userService = require("../service/user-service");
+const { validationResult } = require("express-validator/");
+const ApiError = require("../exceptions/api-error");
 
 class UserController {
   async registration(req, res, next) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest("Validation error", errors.array()));
+      }
       const { email, password } = req.body;
       const userData = await userService.registration(email, password);
       res.cookie("refreshToken", userData.refreshToken, {
