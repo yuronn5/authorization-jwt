8fc49e6d467cef3cb0e1c11db537ffd4fdea8c55
yurii bilous <yurii.b@elogic.com.ua>
2024-08-04 16:23:09 +0200

    refresh token function logic

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -58,6 +58,13 @@ class UserController {
 
   async refresh(req, res, next) {
     try {
+      const { refreshToken } = req.cookies;
+      const userData = await userService.refresh(refreshToken);
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
     } catch (error) {
       next(error);
     }
--- server/service/token-service.js
+++ server/service/token-service.js
@@ -2,31 +2,57 @@ const jwt = require("jsonwebtoken");
 const TokenModel = require("../models/token-model");
 
 class TokenService {
+  generateTokens(payload) {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: "1h",
+    }); // generate access token
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+      expiresIn: "30d",
+    }); // generate refresh token
 
-    generateTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: "1h"}); // generate access token
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: "30d"}); // generate refresh token
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
 
-        return {
-            accessToken,
-            refreshToken
-        }
+  validateAccessToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+      return userData;
+    } catch (error) {
+      return null;
     }
+  }
 
-    async saveToken(userId, refreshToken) {
-        const tokenData = await TokenModel.findOne({user: userId}); // check if token exists
-        if (tokenData) {
-            tokenData.refreshToken = refreshToken; // if token exists
-            return tokenData.save(); // save token in db
-        }
-        const token = await TokenModel.create({user: userId, refreshToken}); // if token doesn't exist
-        return token; // create token in db
+  validateRefreshToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+      return userData;
+    } catch (error) {
+      return null;
     }
+  }
 
-    async removeToken(refreshToken) {
-        const tokenData = await TokenModel.deleteOne({refreshToken}); // delete token from db
-        return tokenData; // return token
+  async saveToken(userId, refreshToken) {
+    const tokenData = await TokenModel.findOne({ user: userId }); // check if token exists
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken; // if token exists
+      return tokenData.save(); // save token in db
     }
+    const token = await TokenModel.create({ user: userId, refreshToken }); // if token doesn't exist
+    return token; // create token in db
+  }
+
+  async removeToken(refreshToken) {
+    const tokenData = await TokenModel.deleteOne({ refreshToken }); // delete token from db
+    return tokenData; // return token
+  }
+
+  async findToken(refreshToken) {
+    const tokenData = await TokenModel.findOne({ refreshToken }); // check if token exists
+    return tokenData;
+  }
 }
 
-module.exports = new TokenService()
+module.exports = new TokenService();
