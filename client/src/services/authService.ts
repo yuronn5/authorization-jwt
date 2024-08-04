d9157154ca60c1e496b514728c75df2461da4982
yurii bilous <yurii.b@elogic.com.ua>
2024-08-04 17:40:22 +0200

    auth and user services

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,9 +1,23 @@
-import $api from "../http"
-import { AxiosResponse } from "axios"
-import { AuthResponce } from "../models/responce/AuthResponce"
+import $api from "../http";
+import { AxiosResponse } from "axios";
+import { AuthResponce } from "../models/responce/AuthResponce";
 
 export default class AuthService {
-    static async login(email: string, password: string): Promise<AxiosResponse<AuthResponce>> {
-        return $api.post<AuthResponce>("/login", { email, password })
-    }
+  static async login(
+    email: string,
+    password: string
+  ): Promise<AxiosResponse<AuthResponce>> {
+    return $api.post<AuthResponce>("/login", { email, password });
+  }
+
+  static async registration(
+    email: string,
+    password: string
+  ): Promise<AxiosResponse<AuthResponce>> {
+    return $api.post<AuthResponce>("/registration", { email, password });
+  }
+
+  static async logout(): Promise<void> {
+    return $api.post("/logout");
+  }
 }
