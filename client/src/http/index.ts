57e2016f910cdc8bb5a3ba6ad91c1c76c0d2a226
yurii bilous <yurii.b@elogic.com.ua>
2024-08-04 19:17:59 +0200

    fixeng issue with refresh token

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from "axios";
-
+import { AuthResponse } from "../models/response/AuthResponse";
+import { config } from "process";
 export const API_URL = `http://localhost:3001/api`;
 
 const $api = axios.create({
@@ -12,5 +13,31 @@ $api.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   return config;
 });
+
+$api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status == 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
+          withCredentials: true,
+        });
+        localStorage.setItem("token", response.data.accessToken);
+        return $api.request(originalRequest);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    throw error;
+  }
+);
 
 export default $api;
