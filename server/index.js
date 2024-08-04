b9e4510ebd5de64aa3bc8e62385a5d239442532f
yurii bilous <yurii.b@elogic.com.ua>
2024-08-04 10:34:50 +0200

    created env file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 const start = async () => {
